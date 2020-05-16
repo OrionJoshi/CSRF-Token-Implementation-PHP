8807b3d8f53c226ef705534091c1a73e78a09a6a
khageshwor <khageshwarjoshi03@gmail.com>
2020-05-16 18:26:12 +0545

    Check database connection

--- includes/functions.php
+++ includes/functions.php
@@ -2,6 +2,12 @@
     
     function get_connection(){
         $connection = mysqli_connect('localhost','root','','csrf');
+
+        if(!$connection){
+            die('Failed to connect db');
+        }
+
+        return $connection;
     }
 
 ?>
