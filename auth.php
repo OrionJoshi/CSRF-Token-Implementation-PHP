1c1f30df1113d98eeb126dc9778965c7a4491a95
khageshwor <khageshwarjoshi03@gmail.com>
2020-05-16 18:32:54 +0545

    Authenticate function add

--- auth.php
+++ auth.php
@@ -6,7 +6,10 @@
 
     if(isset($_POST['submit'])){
         $email = mysqli_real_escape_string($_POST['email']);
-        $pass  = mysqli_real_escape_string($_POST['password']);
+        $password  = mysqli_real_escape_string($_POST['password']);
+
+        //Authentication
+        $authenticated = authenticate($email, $password);
     }
 
 ?>
