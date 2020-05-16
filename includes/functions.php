61ff247d7312a43dd8b65e20fa867176f3a11154
khageshwor <khageshwarjoshi03@gmail.com>
2020-05-16 19:11:00 +0545

    Defination of authenticate function add

--- includes/functions.php
+++ includes/functions.php
@@ -10,6 +10,36 @@
         return $connection;
     }
 
+    function authenticate($email, $password){
+
+        $connection = get_connection();
+
+        $sql = "SELECT * FROM users WHERE email = ?";
+        $statment = mysqli_prepare($connection, $sql);
+
+        if(is_object($statment)) {
+
+            mysqli_stmt_bind_param($statment, 's', $email);
+            mysqli_stmt_bind_result($statment, $db_user_id, $db_user_email, $db_user_password);
+            mysqli_stmt_execute($statment);
+            mysqli_stmt_fetch($statment);
+
+            if(mysqli_stmt_num_rows($statment)){
+
+                if(password_verify($password, $db_user_password)){
+                    return ['loggedin' => true, 'email' => $db_user_email]
+                }
+                return false;
+            }   
+
+            return false;
+
+        }else{
+            echo 'Failed to create statment';
+        }
+
+    }
+
     function debug($arg){
         echo '<pre>';
         print_r($arg);
