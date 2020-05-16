ce1db4d8d33e96c1e51dfff276d8cc260350a2af
khageshwor <khageshwarjoshi03@gmail.com>
2020-05-16 19:52:00 +0545

    Authenticated user information display

--- profile.php
+++ profile.php
@@ -6,3 +6,24 @@
     }
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="
+    sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+</head>
+<body>
+        <div class="container">
+            <div class="row align-items-center" style="min-height:100vh;">
+                <div class="col-md-6 offset-md-3">
+                    <h1>My Profile page</h1>
+                    <p class="alert alert-success"><strong>Welcome: </strong><?= $_SESSION['user']['email']; ?></p>
+                </div>
+            </div>
+        </div>
+</body>
+</html>
